77beec65b0706817d8220626a061357412f1793f
Unknown <maryshipkova@gmail.com>
2018-05-18 19:25:00 +0300

    some services

--- peer-review/src/app/system/shared/services/review.service.ts
+++ peer-review/src/app/system/shared/services/review.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@angular/core';
+import {Api} from '../../../common/core/api';
+import {SolutionModel} from '../../../common/models/solution.model';
+import {Observable} from 'rxjs/Observable';
+import {HttpClient} from '@angular/common/http';
+import {ReviewModel} from '../../../common/models/review.model';
 
 @Injectable()
-export class ReviewService {
+export class ReviewService extends Api {
 
-  constructor() { }
+  constructor(public http: HttpClient) {
+    super(http);
+  }
+
+  createReview(token: string, review: ReviewModel): Observable<ReviewModel> {
+    return this.post('url', [token, review]);
+  }
+
+  getReviewById(token: string, reviewId: number): Observable<ReviewModel> {
+    return this.get('url');
+  }
+
+  getReviewListBySolution(token: string, solutionId: number, userId: number): Observable<ReviewModel[]> {
+    return this.get(`tasks/GetBySolution/${solutionId}?TokenData=${token}&UserId=${userId}`);
+  }
 
 }
--- peer-review/src/app/system/shared/services/solution.service.ts
+++ peer-review/src/app/system/shared/services/solution.service.ts
@@ -1,8 +1,27 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Api} from '../../../common/core/api';
+import {Observable} from 'rxjs/Observable';
+import {TaskModel} from '../../../common/models/task.model';
+import {HttpClient} from '@angular/common/http';
+import {SolutionModel} from '../../../common/models/solution.model';
 
 @Injectable()
-export class SolutionService {
+export class SolutionService extends Api {
 
-  constructor() { }
+  constructor(public http: HttpClient) {
+    super(http);
+  }
+
+  createSolution(token: string, solution: SolutionModel): Observable<SolutionModel> {
+    return this.post('url', [token, solution]);
+  }
+
+  getSolutionById(token: string, solutionId: number): Observable<SolutionModel> {
+    return this.get('url');
+  }
+
+  getSolutionListByTask(token: string, taskId: number, userId: number): Observable<SolutionModel[]> {
+    return this.get(`tasks/GetByTask/${taskId}?TokenData=${token}&UserId=${userId}`);
+  }
 
 }
